a105515fab68f0f222ba20ca0b67adcf5e39bb55
bukaixyz <85857425+bukaixyz@users.noreply.github.com>
2022-05-06 19:09:33 +0800

    Add files via upload

---
+++ Web/initLeanCloud.js
@@ -0,0 +1,7 @@
+// LeanCloud - 初始化 - 将这里的 appId 、 appKey 和 serverURL 替换成自己的应用数据
+// https://leancloud.cn/docs/sdk_setup-js.html#hash14962003
+const appId = "8Xr0CiArhKDwuukQJ82CFEOi-MdYXbMMI";
+const appKey = "aIQooCoNMARDyfGSQn0cUqcC";
+const serverURL = "https://8xr0ciar.api.lncldglobal.com";
+
+LC.init({ appId, appKey, serverURL });
